148981e28b741a3c2f76bc1ca9a75ad3b0826455
Demeter Dzadik <demeterdzadik@gmail.com>
2023-11-11 18:02:19 +0100

    Cleanup: Comments, var names, etc.

--- source/main.lua
+++ source/main.lua
@@ -1,115 +1,88 @@
--- Name this file `main.lua`. Your game can use multiple source files if you wish
--- (use the `import "myFilename"` command), but the simplest games can be written
--- with just `main.lua`.
-
--- You'll want to import these in just about every project you'll work on.
-
 import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
+gfx = playdate.graphics
 
--- Declaring this "gfx" shorthand will make your life easier. Instead of having
--- to preface all graphics calls with "playdate.graphics", just use "gfx."
--- Performance will be slightly enhanced, too.
--- NOTE: Because it's local, you'll have to do it in every .lua source file.
+function initialize()
+    -- Initialize global constants and permanent entities like the player.
 
-local gfx <const> = playdate.graphics
+    default_arm_length = 100
+    max_arm_length = 180
+    min_arm_length = 20
+    arm_extend_speed = 2
 
-local default_arm_length = 100
-local max_arm_length = 180
-local min_arm_length = 20
-local arm_extend_speed = 2
+    alarm_touch_radius = 5
 
-local alarm_touch_radius = 5
+    left_arm_x, left_arm_y = 120, 120
+    right_arm_x, right_arm_y = 280, 120
 
-local left_arm_x, left_arm_y = 120, 120
-local right_arm_x, right_arm_y = 280, 120
-
-local left_arm_sign = -1
-local right_arm_sign = 1
-
--- A function to set up our game environment.
-
-function myGameSetUp()
-    -- Set up the player sprite.
-    -- The :setCenter() call specifies that the sprite will be anchored at its center.
-    -- The :moveTo() call moves our sprite to the center of the display.
+    left_arm_sign = -1
+    right_arm_sign = 1
 
     -- Which arm is active, left (true) or right (false).
-    is_left = false
+    is_left_arm_active = false
 
     local image_hand_left = gfx.image.new("images/hand_left")
     local image_hand_right = gfx.image.new("images/hand_left")
 
-    -- Left hand
+    -- Permanent sprites
+    -- Left hand.
     sprite_hand_l = gfx.sprite.new()
     sprite_hand_l:setImage(image_hand_left)
-	sprite_hand_l:setCenter(1.0, 0.5)
+    sprite_hand_l:setCenter(1.0, 0.5)
     sprite_hand_l:moveTo(left_arm_x-default_arm_length, left_arm_y)
     sprite_hand_l:add()
-    
-    -- Right hand
+
+    -- Right hand.
     sprite_hand_r = gfx.sprite.new()
     sprite_hand_r:setImage(image_hand_right)
-	sprite_hand_r:setCenter(1.0, 0.5)
+    sprite_hand_r:setCenter(1.0, 0.5)
     sprite_hand_r:moveTo(right_arm_x+default_arm_length, right_arm_y)
     sprite_hand_r:add()
 
-    -- Left arm data.
+    -- Left arm. - TODO: Probably needs to be a sprite, otherwise drawing is out of sync.
     player_arm_l = playdate.geometry.lineSegment.new(0, 0, -default_arm_length, 0)
     player_arm_l_angle = 0
 
-    -- Right arm data.
+    -- Right arm.
     player_arm_r = playdate.geometry.lineSegment.new(0, 0, default_arm_length, 0)
     player_arm_r_angle = 0
 
     -- Active and inactive arms.
-    player_arm = player_arm_r
-    player_arm_rest = player_arm_l
+    active_arm = player_arm_r
+    inactive_arm = player_arm_l
 
-    -- Alarm clock data.
+    -- Alarm clock - TODO: Needs ability to have more than one in existence at a time.
     local image_alarm = gfx.image.new("images/alarm")
     sprite_alarm = gfx.sprite.new()
     sprite_alarm:setImage(image_alarm)
     sprite_alarm:setVisible(true)
     sprite_alarm:add()
     alarm_clock_bubble_radius = 0.0
-
 end
+initialize()
 
--- Now we'll call the function above to configure our game.
--- After this runs (it just runs once), nearly everything will be
--- controlled by the OS calling `playdate.update()` 30 times a second.
-
-myGameSetUp()
-
--- `playdate.update()` is the heart of every Playdate game.
--- This function is called right before every frame is drawn onscreen.
--- Use this function to poll input, run game logic, and move sprites.
 
 function playdate.update()
+    -- Called before every frame is drawn.
 
-    -- Poll the d-pad and move our player accordingly.
-    -- (There are multiple ways to read the d-pad; this is the simplest.)
-    -- Note that it is possible for more than one of these directions
-    -- to be pressed at once, if the user is pressing diagonally.
-
-    -- Default drawing context.
     gfx.setLineWidth(2)
+    -- TODO: sprite.update() should be at the end for less input lag, but it wipes non-sprite drawings.
     gfx.sprite.update()
 
-    -- If crank is docked, player is happily sleeping.
+    -- If crank is docked, pause the game.
     if playdate.isCrankDocked() then
         return
     end
 
-    -- If no alarm clock, gives a chance to trigger it.
-    -- Else gitter it around.
+    -- If no alarm clock, give a chance to trigger it.
+    -- Else jitter it around.
     if not sprite_alarm:isVisible() then
         if math.random(0, 256) > 250 then
             sprite_alarm:setVisible(true)
+            -- TODO IMPORTANT: Alarms can spawn in unreachable places!! Need to clamp between arm min/max radius!
             sprite_alarm:moveTo(math.random(50, 350), math.random(50, 190))
         end
     else
@@ -118,48 +91,48 @@ function playdate.update()
 
     -- If A or B button is pressed, define active arm.
     if playdate.buttonIsPressed( playdate.kButtonA ) then
-        is_left = false
+        is_left_arm_active = false
     end
     if playdate.buttonIsPressed( playdate.kButtonB ) then
-        is_left = true
+        is_left_arm_active = true
     end
 
     -- Assign active and resting arms.
-    if is_left then
-        player_arm = player_arm_l
-        player_arm_rest = player_arm_r
+    if is_left_arm_active then
+        active_arm = player_arm_l
+        inactive_arm = player_arm_r
     else
-        player_arm = player_arm_r
-        player_arm_rest = player_arm_l
+        active_arm = player_arm_r
+        inactive_arm = player_arm_l
     end
 
     -- Handle active arm length.
     -- If button is pressed, actively lengthen/shorten it.
     -- Else, automatically move back towards rest/default length (also for inactive arm).
     if playdate.buttonIsPressed( playdate.kButtonRight ) then
-        player_arm.x2 += arm_extend_speed
+        active_arm.x2 += arm_extend_speed
     elseif playdate.buttonIsPressed( playdate.kButtonLeft ) then
-        player_arm.x2 += -arm_extend_speed
-    elseif math.abs(player_arm.x2) ~= default_arm_length then
-        if player_arm.x2 > 100 then
-            player_arm.x2 -= arm_extend_speed
-        elseif player_arm.x2 > 0 then
-            player_arm.x2 += arm_extend_speed
-        elseif player_arm.x2 < -default_arm_length then
-            player_arm.x2 += arm_extend_speed
-        elseif player_arm.x2 < 0 then
-            player_arm.x2 -= arm_extend_speed
+        active_arm.x2 += -arm_extend_speed
+    elseif math.abs(active_arm.x2) ~= default_arm_length then
+        if active_arm.x2 > 100 then
+            active_arm.x2 -= arm_extend_speed
+        elseif active_arm.x2 > 0 then
+            active_arm.x2 += arm_extend_speed
+        elseif active_arm.x2 < -default_arm_length then
+            active_arm.x2 += arm_extend_speed
+        elseif active_arm.x2 < 0 then
+            active_arm.x2 -= arm_extend_speed
         end
     end
-    if math.abs(player_arm_rest.x2) ~= default_arm_length then
-        if player_arm.x2 > default_arm_length then
-            player_arm.x2 -= arm_extend_speed
-        elseif player_arm.x2 > 0 then
-            player_arm.x2 += arm_extend_speed
-        elseif player_arm.x2 < -default_arm_length then
-            player_arm.x2 += arm_extend_speed
-        elseif player_arm.x2 < 0 then
-            player_arm.x2 -= arm_extend_speed
+    if math.abs(inactive_arm.x2) ~= default_arm_length then
+        if active_arm.x2 > default_arm_length then
+            active_arm.x2 -= arm_extend_speed
+        elseif active_arm.x2 > 0 then
+            active_arm.x2 += arm_extend_speed
+        elseif active_arm.x2 < -default_arm_length then
+            active_arm.x2 += arm_extend_speed
+        elseif active_arm.x2 < 0 then
+            active_arm.x2 -= arm_extend_speed
         end
     end
 
@@ -176,7 +149,7 @@ function playdate.update()
     end
 
     -- Read angle from the crank.
-    if is_left then
+    if is_left_arm_active then
         player_arm_l_angle -= playdate.getCrankChange()
     else
         player_arm_r_angle += playdate.getCrankChange()
@@ -222,6 +195,8 @@ function playdate.update()
     -- Draw alarm clock, bigger if there is contact with hands.
     if is_contact then
         sprite_alarm:setScale(1.5)
+    else
+        sprite_alarm:setScale(1.0)
     end
 
     gfx.setLineWidth(1)
@@ -232,11 +207,6 @@ function playdate.update()
         alarm_clock_bubble_radius = 0
     end
 
-    -- Call the functions below in playdate.update() to draw sprites and keep
-    -- timers updated. (We aren't using timers in this example, but in most
-    -- average-complexity games, you will.)
-
-    -- gfx.sprite.update()
     playdate.timer.updateTimers()
 
 end
