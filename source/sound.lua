8e00e816f7d62b85ad1a6668336a902ea9cf9608
Francesco Siddi <francesco.siddi@gmail.com>
2023-11-11 22:01:25 +0100

    Sound: Add more sounds

--- source/sound.lua
+++ source/sound.lua
@@ -7,4 +7,12 @@ SOUND = {
   ALARM4 = fp.new("sound/alarm4"),
   ALARM5 = fp.new("sound/alarm5"),
   ALARM6 = fp.new("sound/alarm6"),
+  BG_LOOP_1 = fp.new("sound/background_loop_stage1"),
+  BG_LOOP_2 = fp.new("sound/background_loop_stage2"),
+  BG_LOOP_3 = fp.new("sound/background_loop_stage3"),
+  CAT_MEOW = fp.new("sound/cat_meow"),
+  CURTAIN_CLOSE = fp.new("sound/curtain_close"),
+  SLAP_ALARM = fp.new("sound/slap_alarm"),
+  SLAP_MOSKITO = fp.new("sound/slap_moskito"),
+  WINDOW_OPEN_LOOP = fp.new("sound/window_open_loop"),
 }
